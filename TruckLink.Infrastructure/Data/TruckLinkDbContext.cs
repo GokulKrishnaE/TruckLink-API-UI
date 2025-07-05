883f860432ab7c80049bc178112fd6ae999b5da4
Gokul Krishna E <=>
2025-07-05 20:14:52 +0530

    db context update

--- TruckLink.Infrastructure/Data/TruckLinkDbContext.cs
+++ TruckLink.Infrastructure/Data/TruckLinkDbContext.cs
@@ -16,26 +16,9 @@ namespace TruckLink.Infrastructure.Data
         {
             base.OnModelCreating(modelBuilder);
 
-            // Convert everything to snake_case if using PostgreSQL
-            if (Database.ProviderName == "Npgsql.EntityFrameworkCore.PostgreSQL")
-            {
-                foreach (var entity in modelBuilder.Model.GetEntityTypes())
-                {
-                    entity.SetTableName(ToSnakeCase(entity.GetTableName()));
-
-                    foreach (var property in entity.GetProperties())
-                        property.SetColumnName(ToSnakeCase(property.GetColumnName()));
-
-                    foreach (var key in entity.GetKeys())
-                        key.SetName(ToSnakeCase(key.GetName()));
-
-                    foreach (var fk in entity.GetForeignKeys())
-                        fk.SetConstraintName(ToSnakeCase(fk.GetConstraintName()));
-
-                    foreach (var index in entity.GetIndexes())
-                        index.SetDatabaseName(ToSnakeCase(index.GetDatabaseName()));
-                }
-            }
+            modelBuilder.Entity<User>().ToTable("users");
+            modelBuilder.Entity<Job>().ToTable("jobs");
+            modelBuilder.Entity<JobInterest>().ToTable("job_interests");
 
             // Custom Fluent Configurations
             modelBuilder.Entity<User>()
@@ -53,13 +36,6 @@ namespace TruckLink.Infrastructure.Data
                 .WithMany(u => u.PostedJobs)
                 .HasForeignKey(j => j.CreatedByUserId)
                 .OnDelete(DeleteBehavior.Restrict);
-        }
-
-        // Convert PascalCase to snake_case
-        private static string ToSnakeCase(string input)
-        {
-            return string.Concat(input.Select((ch, i) =>
-                i > 0 && char.IsUpper(ch) ? "_" + ch : ch.ToString())).ToLower();
         }
     }
 }
